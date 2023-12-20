726a82f858419c642e569b9744c65f48ceaf5b42
Subhajit <022subha@gmail.com>
2023-12-20 14:16:49 +0530

    week-3/02-jwt assignment done

--- assignments/week-3/02-jwt/index.js
+++ assignments/week-3/02-jwt/index.js
@@ -1,6 +1,5 @@
-const jwt = require('jsonwebtoken');
-const jwtPassword = 'secret';
-
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret";
 
 /**
  * Generates a JWT for a given username and password.
@@ -14,7 +13,10 @@ const jwtPassword = 'secret';
  *                        the password does not meet the length requirement.
  */
 function signJwt(username, password) {
-    // Your code here
+  // Your code here
+  const isValid = username.includes("@");
+  if (!isValid || password.length < 6) return null;
+  return jwt.sign({ username, password }, jwtPassword);
 }
 
 /**
@@ -26,7 +28,12 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+  // Your code here
+  try {
+    return jwt.verify(token, jwtPassword) ? true : false;
+  } catch (error) {
+    return false;
+  }
 }
 
 /**
@@ -37,9 +44,9 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+  // Your code here
+  return jwt.decode(token, jwtPassword) ? true : false;
 }
-
 
 module.exports = {
   signJwt,
